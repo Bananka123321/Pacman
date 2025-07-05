119f35224f4d4e7c7b43efb5c41cde20e4bb1988
Ivan <vanushabrend@gmail.com>
2025-07-05 20:06:19 +0700

    Изменена логика управления персонажем, реализована возможность добавления, изменения и загрузка карт

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,7 +1,39 @@
 #include <windows.h>
+#include "Controller.h"
+#include <iostream>
+#include "Maps.h"
+#include "Game.h"
 
 void gotoxy(int x, int y)
 {
     COORD coord = {(short)x, (short)y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+bool IsWall(int x, int y)
+{
+    return Level[y][x] == '#';
+}
+
+bool Movement(int &x, int &y)
+{
+while(!(GetAsyncKeyState(VK_ESCAPE) & 0x8000))
+    {
+        gotoxy(x, y);
+        std::cout << "@";
+
+        Sleep(100);
+
+        gotoxy(x, y);
+        std::cout << " ";
+
+        if (GetAsyncKeyState(VK_UP) & 0x8000 && !IsWall(x, y - 1))
+            y--;
+        if (GetAsyncKeyState(VK_DOWN) & 0x8000 && !IsWall(x, y + 1))
+            y++;
+        if (GetAsyncKeyState(VK_LEFT) & 0x8000 && !IsWall(x - 1, y))
+            x--;
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8000 && !IsWall(x + 1, y))
+            x++;
+    }
 }
