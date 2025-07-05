9a129ab9acf9975867f23b7a80cb061974a7f0a8
Pomidorchik03 <london-266@mail.ru>
2025-07-05 12:11:15 +0800

    Разделение функционала по файлам

--- main.cpp
+++ main.cpp
@@ -3,36 +3,10 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include "src/Game.h"
+#include "src/Controller.h"
 
-void gotoxy(int x, int y)
-{
-    COORD coord = {(short)x, (short)y};
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
 
-int choice_user()
-{
-    bool choice = 0; // Выбор пользователя 0 - PLAY, 1 - EXIT
-    while(!(GetAsyncKeyState(VK_RETURN) & 0x8000 || (GetAsyncKeyState(VK_ESCAPE) & 0x8000))) // Выход из цикла на ESC, ENTER
-    {
-        gotoxy(0,choice);
-        std::cout << ">";
-        Sleep(100);
-        gotoxy(0,choice);
-        std::cout << " ";
-        
-        if ((GetAsyncKeyState(VK_UP) & 0x8000) || (GetAsyncKeyState(VK_DOWN) & 0x8000)) 
-            choice = !choice;
-    }
-    return choice;                      
-}
-
-void menu()
-{
-    std::cout << "  PLAY" << "\n";
-    std::cout << "  EXIT";
-    choice_user();
-}
 
 std::vector<std::string> level = {
     "####################",
@@ -43,7 +17,6 @@ std::vector<std::string> level = {
 
 int main()
 {
-
     menu();
     system("cls");
     for (int i = 0; i < std::size(level); i++)
